6e3b08069610634426cc642fe13c27756f927689
Thomas Sluijs <529181@student.glu.nl>
2020-02-14 16:16:38 +0100

    Added HealthScript

---
+++ Platformer/Assets/Code/Player/HealthScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+    public int HealthPoints;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            TakeDamage();
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Healing();
+        }
+
+        print(HealthPoints);
+    }
+
+
+    public void TakeDamage()
+    {
+        if (HealthPoints <= 1)
+        {
+            DestroyObject(this);
+        }
+        else
+        {
+            HealthPoints--;
+        }
+    }
+
+    public void Healing()
+    {
+        HealthPoints++;
+    }
+}
