355203c97f031c848ae88ead331b1190cc6e691d
Thomas Sluijs <529181@student.glu.nl>
2020-02-19 12:47:53 +0100

    added Real AI

---
+++ Platformer/Assets/Code/EnemyAI/EnemyScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] float MoveSpeed;
+    public bool MoveRight;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (MoveRight)
+        {
+            rb.AddForce(new Vector2((+MoveSpeed), 0));
+            //rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y);
+        }
+        else
+        {
+            rb.AddForce(new Vector2((-MoveSpeed), 0));
+            //rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y);
+        }
+    }
+}
---
+++ Platformer/Assets/Code/EnemyAI/Floorchecker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floorchecker : MonoBehaviour
+{
+    EnemyScript Enemy;
+
+    private void Start()
+    {
+        Enemy = GetComponentInParent<EnemyScript>();
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Environment")
+        {
+            Enemy.MoveRight = !Enemy.MoveRight;
+        }
+    }
+}
